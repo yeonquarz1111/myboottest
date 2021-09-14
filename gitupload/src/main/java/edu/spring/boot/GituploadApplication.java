7b14b56794d982d5bbd89f8a86f1f1416688a926
yeonquarz1111 <syouo@naver.com>
2021-09-14 14:42:17 +0900

    1 commit

--- gitupload/src/main/java/edu/spring/boot/GituploadApplication.java
+++ gitupload/src/main/java/edu/spring/boot/GituploadApplication.java
@@ -8,7 +8,7 @@ public class GituploadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GituploadApplication.class, args);
-		System.out.println("hello github");
+		System.out.println("hello github~");
 	}
 
 }
